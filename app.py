26397b42c67d1cbb31758908e1e6a485fca55379
Syahruddin <syahruddinr@gmail.com>
2019-12-02 08:50:07 +0700

    frontend tanpa ui/ux

--- app.py
+++ app.py
@@ -1,57 +1,25 @@
-from flask import Flask, request, jsonify, render_template
+from flask import Flask, request, jsonify, render_template, redirect, url_for
 import requests
 app = Flask(__name__)
 
-@app.route('/getmsg/', methods=['GET'])
-def respond():
-    # Retrieve the name from url parameter
-    name = request.args.get("name", None)
-
-    # For debugging
-    print(f"got name {name}")
-
-    response = {}
-
-    # Check if user sent a name at all
-    if not name:
-        response["ERROR"] = "no name found, please send a name."
-    # Check if the user entered a number not a name
-    elif str(name).isdigit():
-        response["ERROR"] = "name can't be numeric."
-    # Now the user entered a valid name
-    else:
-        response["MESSAGE"] = f"Welcome {name} to our awesome platform!!"
-
-    # Return the response in json format
-    return jsonify(response)
-
-@app.route('/post/', methods=['POST'])
-def post_something():
-    param = request.form.get('name')
-    print(param)
-    # You can add the test cases you made in the previous function, but in our case here you are just testing the POST functionality
-    if param:
-        return jsonify({
-            "Message": f"Welcome {name} to our awesome platform!!",
-            # Add this option to distinct the POST request
-            "METHOD" : "POST"
-        })
-    else:
-        return jsonify({
-            "ERROR": "no name found, please send a name."
-        })
-
-# A welcome message to test our server
 @app.route('/')
 def index():
-    return render_template('index.html', title='Yasuomid - LOL Analysis App')
+    return render_template('index.html')
 
-@app.route('/matchanal', methods=['GET'])
-def show():
-    if 'match_id' in request.args:
-        match_id = int(request.args['match_id'])
+@app.route('/input', methods=['POST','GET'])
+def input():
+    if request.method == 'POST':
+      gameid = request.form['gameid']
+      return redirect(url_for('matchanal',idgame = gameid))
     else:
-        return "Error: No match field provided. Please specify an match."
+      gameid = request.args.get('gameid')
+      return redirect(url_for('matchanal',idgame = gameid))
+
+    
+
+@app.route('/matchanal/<idgame>', methods=['GET'])
+def matchanal(idgame):
+    match_id = idgame
     
     urlmatch = "https://yasapi.herokuapp.com/match/matches_info?match_id=" + str(match_id)
     urlparty = "https://yasapi.herokuapp.com/match/participant?match_id=" + str(match_id)
